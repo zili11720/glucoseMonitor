515c008c076d78a3f299bf6b6a15fd46a21c8cbd
zili11720 <zili11720@gmail.com>
2024-10-07 20:23:42 +0300

    technical files names changes

--- routes/loginRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const loginController = require('../controllers/loginController.js');
-
-// Route for login post
-router.post('/login', loginController.login);
-
-module.exports = router;
