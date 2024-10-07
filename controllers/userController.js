bf653ff49c0ecb65d7373634a089b95c58b01fe1
zili11720 <zili11720@gmail.com>
2024-10-08 02:06:36 +0300

    home and contact ejs pages

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ async function login(req, res) {
   
     if (isValidUser) {
       // If valid, redirect to the next page 
-      res.redirect('./home');
+      res.redirect('/home');
     } else {
       // If invalid, send back an error message without 
       res.status(401).send({ error: 'Invalid username or password' });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,4 +5,13 @@ const loginController = require('../controllers/userController.js');
 // Route for login post
 router.post('/login', loginController.login);
 
+router.get('/home', (req, res) => {
+    res.render('pages/home');  // Render the home page
+});
+
+router.get('/contact', (req, res) => {
+    res.render('pages/contact');  //  זה כאן זמניתץ להעיף את זה מכאן אחכ
+});
+
+
 module.exports = router;
