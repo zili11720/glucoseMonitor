7b5d9e7a4f68940a6964d512fb55bf4b1c68fb66
zili11720 <zili11720@gmail.com>
2024-10-10 13:15:04 +0300

    app.js user's session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require('express-session'); //A session for managing user account- access to user's id after login
 const userRouter = require("./routes/userRoutes"); // User-related routes
 const newMealRouter = require("./routes/newMealRoutes"); // Uploaded user's picture-related routes
 const dateRoutes = require("./routes/dateRoutes"); // Date-related routes (including holiday API integration)
@@ -10,6 +11,13 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret:'mySuperSecretKeyThatIsLongAndRandom12345',
+  resave: false, // Forces session to be saved back to the session store, even if unmodified
+  saveUninitialized: true, // Forces a session that is "uninitialized" to be saved to the store
+  cookie: { secure: false } // Set to true if using HTTPS
+}));
 
 app.get("/", (req, res) => {
   res.render("pages/index");
